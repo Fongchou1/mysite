ae5d80495e0e66b4cc2fa1ba60e168b0ea069fe8
choumac <choumac@choumacde-mbp.localdomain>
2021-03-16 13:33:37 +0100

    debug and recode for node.js

--- index.js
+++ index.js
@@ -14,6 +14,17 @@ app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/contact", function(req, res){
+  res.sendFile(__dirname + "/contact.html");
+});
+
+app.get("/photography", function(req, res){
+  res.sendFile(__dirname + "/photography.html");
+});
+
+app.get("/webProjects", function(req, res){
+  res.sendFile(__dirname + "/webProjects.html");
+});
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Server started at port 3000.");
