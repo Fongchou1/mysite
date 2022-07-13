164a78b1fcdd46337df93de2517e9707512d9aa9
Fong Feng-En Chou <m82530@gmail.com>
2022-07-13 04:12:35 +0200

    connect airtable and set up object structure and connect with ejs and express

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 var path = require('path');
 var ejs = require('ejs');
+const cors = require("cors");
+var Airtable = require('airtable');
+const bp = require("body-parser");
+// const ffetch = require("node-fetch");
 // var scripts = require(__dirname + '/public/js/scripts.js');
 
 const app = express();
@@ -9,16 +13,110 @@ const { JSDOM } = require( "jsdom" );
 const { window } = new JSDOM( "" );
 const $ = require( "jquery" )( window );
 
-
+app.use(cors());
+app.use(bp.urlencoded({ extended: false }));
+app.use(bp.json());
+app.use(require("morgan")("dev"));
 
 app.use(express.static("public"));
 app.set('views', path.join(__dirname, 'views'));
 
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res){
+// get airtable
+var airtable = new Airtable({apiKey: 'keyUVmbar6wiZjmrx'});
+var photographyBase = airtable.base('appSfjdiCQWegHcDt');
+var landingPageBase = airtable.base('appKYRnm4BMdQAbEs');
+var footerBase = airtable.base('appW37UpLfSwnHVtS');
+
+function readFields( outputObject , records ){
+  for( let i = 0; i < records.length; i++){
+    let thisRecord = records[i];
+    outputObject[thisRecord.fields.title] = thisRecord.fields;
+  }
+}
+
+app.get("/", async function(req, res){
   // res.sendFile(__dirname + "/index.html");
-  res.render("index");
+  
+  let rootTexts = {};
+  let links = {};
+  let images = {};
+  let videos = {};
+  let projects = {};
+
+  // airtable
+  await landingPageBase('root-texts').select({
+    view: 'DB'
+  }).all(function(err, records) {
+
+    if(err) { console.error(err); return; }
+
+    // root texts fetched from airtable
+    readFields(rootTexts, records);
+    
+    // move forward to fetch another table
+    landingPageBase('links').select({
+      view: 'DB'
+    }).all(function(err, records) {
+
+      if(err) { console.error(err); return; }
+      
+      // links fetched from airtable
+      readFields(links, records);
+
+      // move forward to fetch another table
+      landingPageBase('images').select({
+        view: 'DB'
+      }).all(function(err, records) {
+  
+        if(err) { console.error(err); return; }
+        
+        // images fetched from airtable
+        readFields(images, records);
+
+        //move forward to fetch another table
+        landingPageBase('videos').select({
+          view: 'DB'
+        }).all(function(err, records) {
+    
+          if(err) { console.error(err); return; }
+          
+          // videos fetched from airtable
+          readFields(videos, records);
+  
+          //move forward to fetch another table
+          landingPageBase('projects').select({
+            view: 'DB'
+          }).all(function(err, records) {
+      
+            if(err) { console.error(err); return; }
+            
+            // projects fetched from airtable
+            readFields(projects, records);
+    
+            // fetched all required data and send
+            res.render("index",
+              {
+                rootTexts: rootTexts,
+                links: links,
+                images: images,
+                videos: videos,
+                projects: projects
+              }
+            );
+      
+          })
+    
+        })
+  
+      })
+
+    })
+
+  })
+  
+
 });
 
 app.get("/contact", function(req, res){
