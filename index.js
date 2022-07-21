cb5e9746a2e84b939f4b9d047a3c53a8e9594667
Fong Feng-En Chou <m82530@gmail.com>
2022-07-21 02:38:06 +0200

    first new website draft done

--- index.js
+++ index.js
@@ -29,6 +29,11 @@ var photographyBase = airtable.base('appSfjdiCQWegHcDt');
 var landingPageBase = airtable.base('appKYRnm4BMdQAbEs');
 var footerBase = airtable.base('appW37UpLfSwnHVtS');
 var e_missionWebBase = airtable.base('apppg3kkNAKUNbBzl');
+var e_missionApplicationBase = airtable.base('appY39fAPDy7WjEQM');
+var tarocchiBase = airtable.base('appELnXp5icqdf1hP');
+var posAppBase = airtable.base('appumdk46LYqsypYu');
+var berlineatsBase = airtable.base('appOMXVGZbdHaqfVx');
+var graphicsBase = airtable.base('app8jeAyfHpy7jXmX');
 
 function readFields( outputObject , records ){
   for( let i = 0; i < records.length; i++){
@@ -153,6 +158,7 @@ app.get("/e-mission-web", function(req, res){
   let rootTexts = {};
   let links = {};
   let sections = {};
+  let images = {};
   let footerRootTexts = {};
   let footerLinks = {};
 
@@ -206,17 +212,474 @@ app.get("/e-mission-web", function(req, res){
             // e-mission web sections fetched from airtable
             readFields(sections, records);
             
-            // fetched all required data and send
-            res.render("project",
-              {
-                rootTexts: rootTexts,
-                links: links,
-                sections: sections,
-                footerRootTexts: footerRootTexts,
-                footerLinks: footerLinks
-              }
-            );
+            // fetch e-mission web images
+            e_missionWebBase('images').select({
+              view: 'DB'
+            }).all(function(err, records) {
+        
+              if(err) { console.error(err); return; }
+              
+              // e-mission web images fetched from airtable
+              readFields(images, records);
+              
+              // fetched all required data and send
+              res.render("project",
+                {
+                  rootTexts: rootTexts,
+                  links: links,
+                  sections: sections,
+                  images: images,
+                  footerRootTexts: footerRootTexts,
+                  footerLinks: footerLinks
+                }
+              );
+        
+            })
+          })
+        })
+      })
+    })
+  })
+});
+
+app.get("/e-mission-application", function(req, res){
+
+  let rootTexts = {};
+  let links = {};
+  let sections = {};
+  let images = {};
+  let footerRootTexts = {};
+  let footerLinks = {};
+
+  //fetch footer root text
+  footerBase('root-texts').select({
+    view: 'DB'
+  }).all(function(err, records) {
+
+    if(err) { console.error(err); return; }
+    
+    // footer root text fetched from airtable
+    readFields(footerRootTexts, records);
+
+    // fetch footer links
+    footerBase('links').select({
+      view: 'DB'
+    }).all(function(err, records) {
+
+      if(err) { console.error(err); return; }
       
+      // footer links fetched from airtable
+      readFields(footerLinks, records);
+
+      // fetch e-mission application root texts
+      e_missionApplicationBase('root-texts').select({
+        view: 'DB'
+      }).all(function(err, records) {
+  
+        if(err) { console.error(err); return; }
+        
+        // e-mission application root texts fetched from airtable
+        readFields(rootTexts, records);
+        
+        // fetch e-mission application links
+        e_missionApplicationBase('links').select({
+          view: 'DB'
+        }).all(function(err, records) {
+    
+          if(err) { console.error(err); return; }
+          
+          // e-mission application links fetched from airtable
+          readFields(links, records);
+          
+          // fetch e-mission application sections
+          e_missionApplicationBase('sections').select({
+            view: 'DB'
+          }).all(function(err, records) {
+      
+            if(err) { console.error(err); return; }
+            
+            // e-mission application sections fetched from airtable
+            readFields(sections, records);
+            
+            // fetch e-mission application images
+            e_missionApplicationBase('images').select({
+              view: 'DB'
+            }).all(function(err, records) {
+        
+              if(err) { console.error(err); return; }
+              
+              // e-mission application images fetched from airtable
+              readFields(images, records);
+              
+              // fetched all required data and send
+              res.render("project",
+                {
+                  rootTexts: rootTexts,
+                  links: links,
+                  sections: sections,
+                  images: images,
+                  footerRootTexts: footerRootTexts,
+                  footerLinks: footerLinks
+                }
+              );
+        
+            })
+          })
+        })
+      })
+    })
+  })
+});
+
+app.get("/tarocchi", function(req, res){
+
+  let rootTexts = {};
+  let links = {};
+  let sections = {};
+  let images = {};
+  let footerRootTexts = {};
+  let footerLinks = {};
+
+  //fetch footer root text
+  footerBase('root-texts').select({
+    view: 'DB'
+  }).all(function(err, records) {
+
+    if(err) { console.error(err); return; }
+    
+    // footer root text fetched from airtable
+    readFields(footerRootTexts, records);
+
+    // fetch footer links
+    footerBase('links').select({
+      view: 'DB'
+    }).all(function(err, records) {
+
+      if(err) { console.error(err); return; }
+      
+      // footer links fetched from airtable
+      readFields(footerLinks, records);
+
+      // fetch tarocchi root texts
+      tarocchiBase('root-texts').select({
+        view: 'DB'
+      }).all(function(err, records) {
+  
+        if(err) { console.error(err); return; }
+        
+        // tarocchi root texts fetched from airtable
+        readFields(rootTexts, records);
+        
+        // fetch tarocchi links
+        tarocchiBase('links').select({
+          view: 'DB'
+        }).all(function(err, records) {
+    
+          if(err) { console.error(err); return; }
+          
+          // tarocchi links fetched from airtable
+          readFields(links, records);
+          
+          // fetch tarocchi sections
+          tarocchiBase('sections').select({
+            view: 'DB'
+          }).all(function(err, records) {
+      
+            if(err) { console.error(err); return; }
+            
+            // tarocchi sections fetched from airtable
+            readFields(sections, records);
+            
+            // fetch tarocchi images
+            tarocchiBase('images').select({
+              view: 'DB'
+            }).all(function(err, records) {
+        
+              if(err) { console.error(err); return; }
+              
+              // tarocchi images fetched from airtable
+              readFields(images, records);
+              
+              // fetched all required data and send
+              res.render("project",
+                {
+                  rootTexts: rootTexts,
+                  links: links,
+                  sections: sections,
+                  images: images,
+                  footerRootTexts: footerRootTexts,
+                  footerLinks: footerLinks
+                }
+              );
+        
+            })
+          })
+        })
+      })
+    })
+  })
+});
+
+app.get("/pos-app", function(req, res){
+
+  let rootTexts = {};
+  let links = {};
+  let sections = {};
+  let images = {};
+  let footerRootTexts = {};
+  let footerLinks = {};
+
+  //fetch footer root text
+  footerBase('root-texts').select({
+    view: 'DB'
+  }).all(function(err, records) {
+
+    if(err) { console.error(err); return; }
+    
+    // footer root text fetched from airtable
+    readFields(footerRootTexts, records);
+
+    // fetch footer links
+    footerBase('links').select({
+      view: 'DB'
+    }).all(function(err, records) {
+
+      if(err) { console.error(err); return; }
+      
+      // footer links fetched from airtable
+      readFields(footerLinks, records);
+
+      // fetch pos app root texts
+      posAppBase('root-texts').select({
+        view: 'DB'
+      }).all(function(err, records) {
+  
+        if(err) { console.error(err); return; }
+        
+        // pos app root texts fetched from airtable
+        readFields(rootTexts, records);
+        
+        // fetch pos app links
+        posAppBase('links').select({
+          view: 'DB'
+        }).all(function(err, records) {
+    
+          if(err) { console.error(err); return; }
+          
+          // pos app links fetched from airtable
+          readFields(links, records);
+          
+          // fetch pos app sections
+          posAppBase('sections').select({
+            view: 'DB'
+          }).all(function(err, records) {
+      
+            if(err) { console.error(err); return; }
+            
+            // pos app sections fetched from airtable
+            readFields(sections, records);
+            
+            // fetch pos app images
+            posAppBase('images').select({
+              view: 'DB'
+            }).all(function(err, records) {
+        
+              if(err) { console.error(err); return; }
+              
+              // pos app images fetched from airtable
+              readFields(images, records);
+              
+              // fetched all required data and send
+              res.render("project",
+                {
+                  rootTexts: rootTexts,
+                  links: links,
+                  sections: sections,
+                  images: images,
+                  footerRootTexts: footerRootTexts,
+                  footerLinks: footerLinks
+                }
+              );
+        
+            })
+          })
+        })
+      })
+    })
+  })
+});
+
+app.get("/berlineats", function(req, res){
+
+  let rootTexts = {};
+  let links = {};
+  let sections = {};
+  let images = {};
+  let footerRootTexts = {};
+  let footerLinks = {};
+
+  //fetch footer root text
+  footerBase('root-texts').select({
+    view: 'DB'
+  }).all(function(err, records) {
+
+    if(err) { console.error(err); return; }
+    
+    // footer root text fetched from airtable
+    readFields(footerRootTexts, records);
+
+    // fetch footer links
+    footerBase('links').select({
+      view: 'DB'
+    }).all(function(err, records) {
+
+      if(err) { console.error(err); return; }
+      
+      // footer links fetched from airtable
+      readFields(footerLinks, records);
+
+      // fetch BerlinEats root texts
+      berlineatsBase('root-texts').select({
+        view: 'DB'
+      }).all(function(err, records) {
+  
+        if(err) { console.error(err); return; }
+        
+        // BerlinEats root texts fetched from airtable
+        readFields(rootTexts, records);
+        
+        // fetch BerlinEats links
+        berlineatsBase('links').select({
+          view: 'DB'
+        }).all(function(err, records) {
+    
+          if(err) { console.error(err); return; }
+          
+          // BerlinEats links fetched from airtable
+          readFields(links, records);
+          
+          // fetch BerlinEats sections
+          berlineatsBase('sections').select({
+            view: 'DB'
+          }).all(function(err, records) {
+      
+            if(err) { console.error(err); return; }
+            
+            // BerlinEats sections fetched from airtable
+            readFields(sections, records);
+            
+            // fetch BerlinEats images
+            berlineatsBase('images').select({
+              view: 'DB'
+            }).all(function(err, records) {
+        
+              if(err) { console.error(err); return; }
+              
+              // BerlinEats images fetched from airtable
+              readFields(images, records);
+              
+              // fetched all required data and send
+              res.render("project",
+                {
+                  rootTexts: rootTexts,
+                  links: links,
+                  sections: sections,
+                  images: images,
+                  footerRootTexts: footerRootTexts,
+                  footerLinks: footerLinks
+                }
+              );
+        
+            })
+          })
+        })
+      })
+    })
+  })
+});
+
+app.get("/graphics", function(req, res){
+
+  let rootTexts = {};
+  let links = {};
+  let sections = {};
+  let images = {};
+  let footerRootTexts = {};
+  let footerLinks = {};
+
+  //fetch footer root text
+  footerBase('root-texts').select({
+    view: 'DB'
+  }).all(function(err, records) {
+
+    if(err) { console.error(err); return; }
+    
+    // footer root text fetched from airtable
+    readFields(footerRootTexts, records);
+
+    // fetch footer links
+    footerBase('links').select({
+      view: 'DB'
+    }).all(function(err, records) {
+
+      if(err) { console.error(err); return; }
+      
+      // footer links fetched from airtable
+      readFields(footerLinks, records);
+
+      // fetch graphics root texts
+      graphicsBase('root-texts').select({
+        view: 'DB'
+      }).all(function(err, records) {
+  
+        if(err) { console.error(err); return; }
+        
+        // graphics root texts fetched from airtable
+        readFields(rootTexts, records);
+        
+        // fetch graphics links
+        graphicsBase('links').select({
+          view: 'DB'
+        }).all(function(err, records) {
+    
+          if(err) { console.error(err); return; }
+          
+          // graphics links fetched from airtable
+          readFields(links, records);
+          
+          // fetch graphics sections
+          graphicsBase('sections').select({
+            view: 'DB'
+          }).all(function(err, records) {
+      
+            if(err) { console.error(err); return; }
+            
+            // graphics sections fetched from airtable
+            readFields(sections, records);
+            
+            // fetch graphics images
+            graphicsBase('images').select({
+              view: 'DB'
+            }).all(function(err, records) {
+        
+              if(err) { console.error(err); return; }
+              
+              // graphics images fetched from airtable
+              readFields(images, records);
+              
+              // fetched all required data and send
+              res.render("graphics",
+                {
+                  rootTexts: rootTexts,
+                  links: links,
+                  sections: sections,
+                  images: images,
+                  footerRootTexts: footerRootTexts,
+                  footerLinks: footerLinks
+                }
+              );
+        
+            })
           })
         })
       })
